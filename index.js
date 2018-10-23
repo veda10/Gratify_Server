778c20a31e6864e979338a96f917a99a166ea464
Syed Jahangir <syedjahangir.p16@iiits.in>
2018-10-23 15:38:19 +0530

    Registration functions updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import cors from 'cors';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
@@ -10,6 +11,7 @@ import models from './models';
 const server = new ApolloServer({ typeDefs, resolvers, context: {models, user: {id:1,}} });
 
 const app = express();
+app.use(cors('*'));
 server.applyMiddleware({ app });
 
 models.sequelize.sync({}).then(() => {
