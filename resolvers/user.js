3732507b7aa6566b58f01b5cab66d43508a75375
Syed Jahangir <syedjahangir.p16@iiits.in>
2018-10-24 12:46:14 +0530

    Login Integrated

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
+import { tryLogin } from '../auth';
  const formatErrors = (e, models) => {
   if (e instanceof models.sequelize.ValidationError) {
     //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
@@ -14,6 +15,8 @@ export default {
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
+    login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
+      tryLogin(email, password, models, SECRET),
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
         if (password.length < 5 || password.length > 100) {
